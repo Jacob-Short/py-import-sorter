d77a4e637130c6d746f5b7fd29ad38be25da16ee
Jacob Short <thatmanjshort@gmail.com>
2021-12-27 11:02:34 -0800

    implemnted ability to sort mutli-line imports

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def create_parser() -> argparse.ArgumentParser:
 
     parser.add_argument("dir", help="name of directory to sort python files")
 
-    print(f'Parser:\n{type(parser)}')
+    print(f"Parser:\n{type(parser)}")
     return parser
 
 
@@ -35,32 +35,76 @@ def sort_imports(py_file, dir) -> list:
         # until matching ')' is found
 
         # temp placeholder for edge case lines
+        multi_line = False
         edge_case_lines = []
 
+        # this takes a lot of time. lets look back into this later
+        # for efficiency
         for index, line in enumerate(all_lines):
+            if line.strip().endswith(")"):
+                edge_case_lines.append(line)
+                multi_line = False
+            if multi_line:
+                edge_case_lines.append(line)
             if line.strip().endswith("("):
                 print(f"This is a edge case: [ {line} ]")
-                sort_edge_cases(all[index:])
-                    # can create separate function and pass the rest of the list
-                        # to create a edge_case_lists
-                        # then can append them all after
+                edge_case_lines.append(line)
+                multi_line = True
+
+        edge_case_lines.append('\n')
+
+        print(f"edge cases:\n{edge_case_lines}")
+
+        # TODO:
+            # edge cases need to be sorted
+
+        # sort_edge_cases(all[index:])
+        # pass the rest of the list && line num
+        # to create a edge_case_lists
+        # then can append them all after
+
+        # OR
+
+        # can create boolean multi_line = False
+        # then can set to true when find the opening '('
+        # then will append all future iterations until
+        # finding the closing ')' then can set back to False
+
+        # TODO:
+        # will need to remove all edge case lines from both
+        # sorted import && non import lists
         sorted_import_names = sorted(
             [
                 line
                 for line in all_lines
-                if line.startswith("import") or line.startswith("from")
+                if line.startswith("import")
+                or line.startswith("from")
+                and line not in edge_case_lines
             ]
         )
+
+        # TODO:
+        # add new line in between reg imports & 'from' imports
+
+        for ind, line in enumerate(sorted_import_names):
+            if line.startswith("import"):
+                sorted_import_names.insert(ind, "\n")
+                break
+
+        sorted_import_names.append("\n")
         non_import_lines = [
             line
             for line in all_lines
-            if not line.startswith("import") and not line.startswith("from")
+            if not line.startswith("import")
+            and not line.startswith("from")
+            and line not in edge_case_lines
         ]
+        non_import_lines.append("\n")
 
         print(f"import lines:\n{sorted_import_names}")
         print(f"non import lines:\n{non_import_lines}")
 
-        result = sorted_import_names + non_import_lines
+        result = sorted_import_names + edge_case_lines + non_import_lines
         # print(f"End Result:\n{result}")
 
     with open(os.path.join(dir, py_file), "w") as wf:
@@ -71,13 +115,13 @@ def sort_imports(py_file, dir) -> list:
 
 
 def sort_edge_cases(remaining_lines) -> list:
-    '''will append next iterations of lines with 
-    ' from pypy import ( 
+    """will append next iterations of lines with
+    ' from pypy import (
         foo,
         bar,
     '
     )
-    '''
+    """
     for line in remaining_lines:
         print(line)
 
