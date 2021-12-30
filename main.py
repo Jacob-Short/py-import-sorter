6862f4bb896b20c9d04ad871795492abf528a96f
Jacob Short <thatmanjshort@gmail.com>
2021-12-29 21:38:00 -0800

    fixed bug where non import statements ending with opening parens getting caught in edge_case list and getting sorted

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import sys
 # for python 3.8 and earlier
 # from typing import List, Set, Dict, Tuple, Optional
 
-
-exit_flag = False
+# TODO:
+# need some type of incrementor for number of py files
+# for readability when sorting
 
 
 def create_parser() -> argparse.ArgumentParser:
@@ -19,12 +20,12 @@ def create_parser() -> argparse.ArgumentParser:
 
     parser.add_argument("dir", help="name of directory to sort python files")
 
-    print(f"Parser:\n{type(parser)}")
     return parser
 
 
-def sort_imports(py_file, dir) -> list:
+def sort_imports(py_file: str, dir: str) -> list:
     """will look at all .py files and sort all imports"""
+
     import_names = []
     with open(os.path.join(dir, py_file)) as f:
         all_lines = f.readlines()
@@ -32,29 +33,43 @@ def sort_imports(py_file, dir) -> list:
         multi_line = False
         edge_case_lines = []
 
-        # this takes a lot of time. lets look back into this later
-        # for efficiency
+        """
+        this section is to account for multi line import statements
+        EX:
+        from user_account.forms import (
+            CreateProfileForm,
+            LoginForm,
+            RegisterForm,
+        )
+        """
+        # TODO:
+        # refactor using regex
         for index, line in enumerate(all_lines):
-            if line.strip().endswith(")"):
-                edge_case_lines.append(line)
-                multi_line = False
-            if multi_line:
-                edge_case_lines.append(line)
-            if line.strip().endswith("("):
+            if line.strip().startswith("from") and line.strip().endswith("("):
                 print(f"This is a edge case: [ {line} ]")
                 edge_case_lines.append(line)
                 multi_line = True
+            elif line.strip().endswith(")") and multi_line:
+                edge_case_lines.append(line)
+                multi_line = False
+            elif multi_line:
+                edge_case_lines.append(line)
 
         print(f"edge cases:\n{edge_case_lines}")
 
-        sorted_edge_case_lines = sorted([line for line in edge_case_lines[1:-1]])
+        """
+        this section is to check if there are any multi-line import statements
+        and if so then to sort them
+        """
+        if len(edge_case_lines) > 1:
+            sorted_edge_case_lines = sorted([line for line in edge_case_lines[1:-1]])
 
-        print(f"sorted edge case lines:\n{sorted_edge_case_lines}")
+            print(f"sorted edge case lines:\n{sorted_edge_case_lines}")
 
-        sorted_edge_case_lines.insert(0, edge_case_lines[0])
-        sorted_edge_case_lines.append(edge_case_lines[-1])
+            sorted_edge_case_lines.insert(0, edge_case_lines[0])
+            sorted_edge_case_lines.append(edge_case_lines[-1])
 
-        print(f"FINAL RESULT OF sorted edge case lines:\n{sorted_edge_case_lines}")
+            print(f"FINAL RESULT OF sorted edge case lines:\n{sorted_edge_case_lines}")
 
         sorted_import_names = sorted(
             [
@@ -66,10 +81,29 @@ def sort_imports(py_file, dir) -> list:
             ]
         )
 
+        """
+        doing this to create new line between reg imports & from
+        import statements
+        Ex:
+            Before:
+
+                import argparse
+                from django import widgets
+                import time
+                from django import forms
+            After:
+            
+                from django import forms
+                from django import widgets
+
+                import argarse
+                importtime
+        """
         for ind, line in enumerate(sorted_import_names):
             if line.startswith("import"):
                 sorted_import_names.insert(ind, "\n")
                 break
+
         sorted_import_names.append("\n")
         non_import_lines = [
             line
@@ -90,17 +124,21 @@ def sort_imports(py_file, dir) -> list:
     with open(os.path.join(dir, py_file), "w") as wf:
         wf.writelines(result)
 
+        # TODO:
+        # before closing the file -- can run flake8 / black
+
     return sorted_import_names
 
 
-def check_for_py_files(directory) -> list:
-    """will look at all .py files and sort all imports"""
+def check_for_py_files(directory: str) -> list:
+    """takes in abs path of directory and will look at all .py files
+    and sort all imports
+    """
     full_path = os.path.abspath(directory)
     py_files = [file for file in os.listdir(full_path) if file.endswith(".py")]
-    for file in py_files:
-        print(f"Found a python file: [ {file} ], starting to sort now...")
+    for file_num, file in enumerate(py_files):
+        print(f"{file_num} -- Found a python file: [ {file} ], starting to sort now...")
         sort_imports(file, directory)
-    print(type(py_files))
     return py_files
 
 
@@ -110,6 +148,8 @@ def main(args):
     parser = create_parser()
     ns = parser.parse_args(args)
 
+    exit_flag = False
+
     directory = ns.dir
 
     if not ns:
@@ -118,9 +158,13 @@ def main(args):
     while not exit_flag:
         try:
             check_for_py_files(directory)
+            exit_flag = True
+        except FileNotFoundError as err:
+            print(f"'{directory}' does not exist. The asolute path is required.")
             break
         except Exception as err:
             print(err)
+            break
 
 
 if __name__ == "__main__":
