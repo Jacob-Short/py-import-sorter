fb9fc84bad2f13a7a902ffcff0d2c07c148954ea
Jacob Short <thatmanjshort@gmail.com>
2021-12-30 19:47:01 -0800

    cleaned up edge case file causing problems within ide -- taken care of within testing

--- tests/edge_case_dir/edge_case_file.py
+++ tests/edge_case_dir/edge_case_file.py
@@ -12,32 +12,3 @@ from ..directory_with_py_files.forms import (
     LoginForm,
     RegisterForm,
 )
-
-
-
-
-
-
-
-
-class Message(models.Model):
-
-    CHOICES = [(1, "True"), (2, "False")]
-    message = models.TextField(max_length=500)
-    creator = models.ForeignKey(
-        User, related_name="%(class)s_author", null=True, on_delete=models.CASCADE
-    )
-    recipient = models.ForeignKey(
-        User, related_name="%(class)s_recipient", null=True, on_delete=models.CASCADE
-    )
-    time_created = models.DateTimeField(default=timezone.now)
-    isNew = models.BooleanField(choices=CHOICES, default=1)
-
-    def __str__(self):
-        return self.message
-
-
-
-
-
-
